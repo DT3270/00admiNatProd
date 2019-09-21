32b0ad58b4be92f0542fe52f0b2778890f588e32
DT3270 <34624867+DT3270@users.noreply.github.com>
2019-09-21 16:42:38 -0300

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-      url: 'mongodb+srv://dt23270:fuiyu001@cluster0-itmdj.mongodb.net:27017/test?retryWrites=true&w=majority'
+      url: 'mongodb://dt23270:fuiyu001@cluster0-itmdj.mongodb.net:27017/test?retryWrites=true&w=majority'
+//    url: 'mongodb+srv://dt23270:fuiyu001@cluster0-itmdj.mongodb.net/test?retryWrites=true&w=majority'
 //    url: 'mongodb://localhost:27017/admiNat'
 }
