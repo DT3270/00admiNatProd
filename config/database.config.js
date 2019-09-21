8580403e0771af04334e3fde05145dacafbf4391
DT3270 <34624867+DT3270@users.noreply.github.com>
2019-09-21 15:11:25 -0300

    instalación express

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
       url: 'mongodb+srv://dt23270:fuiyu001@cluster0-itmdj.mongodb.net/test?retryWrites=true&w=majority'
-//      url: 'mongodb://localhost:27017/admiNat'
+//    url: 'mongodb://localhost:27017/admiNat'
 }
