584bef4fe3719a577a0ae3cbc65e5665ccff47ac
DT3270 <34624867+DT3270@users.noreply.github.com>
2019-09-21 14:15:59 -0300

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-      url: 'mongodb://localhost:27017/admiNat'
-//    url: 'mongodb://dt3270.github.io/Adminat2:27017/admiNat'
+      url: 'mongodb+srv://dt23270:fuiyu001@cluster0-itmdj.mongodb.net/test?retryWrites=true&w=majority'
+//      url: 'mongodb://localhost:27017/admiNat'
 }
