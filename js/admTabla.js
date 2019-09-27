fe9b8b2a8c36f47f1affc0271f855de4533820a5
DT3270 <34624867+DT3270@users.noreply.github.com>
2019-09-27 07:58:07 -0300

    cambiandoACastellano

--- js/admTabla.js
+++ js/admTabla.js
@@ -83,9 +83,40 @@ function crearTabla(tit, tab) {
   tbl.setAttribute("class", "display");
 
   $('#tabla').DataTable( {
+
+    "language": 
+      {
+        "sProcessing":     "Procesando...",
+        "sLengthMenu":     "Mostrar _MENU_ registros",
+        "sZeroRecords":    "No se encontraron resultados",
+        "sEmptyTable":     "Ningún dato disponible en esta tabla",
+        "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_",
+        "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix":    "",
+        "sSearch":         "Buscar:",
+        "sUrl":            "",
+        "sInfoThousands":  ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+          "sFirst":    "Primero",
+          "sLast":     "Último",
+          "sNext":     "Siguiente",
+          "sPrevious": "Anterior"
+                      },
+        "oAria": {
+          "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+          "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                  }      },
+
+    "columnDefs": [
+      {className: "dt-body-center","targets": [0,2,3,4,5,6,7,8]},
+
+    ],
+
     "footerCallback": function ( row, data, start, end, display ) {
-    var api = this.api(), data;
-  
+    var api = this.api(), data;  
+
     // Remove the formatting to get integer data for summation
     var intVal = function ( i ) {
       return typeof i === 'string' ?
@@ -208,6 +239,7 @@ function crearTabla(tit, tab) {
     );
 
   }
+
   } );
 }
 
